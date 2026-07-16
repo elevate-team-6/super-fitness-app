53f9b58c7c4bb8227a8024f0ad8c9df7d100b4d3
Abdelmalek <m7244985@gmail.com>
2026-07-16 10:13:02 +0300

    feat(register): implement register screen UI
    - Design complete registration UI with custom text fields, glass container, and social buttons
    - Add form validation and text controllers for first name, last name, email, and password
    - Handle controller disposal to prevent memory leaks
    - Delete unused dummy.dart file

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -3,13 +3,20 @@ import 'dart:async';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:super_fitness/config/base_ui_handler/ui_event_handler_mixin.dart';
+import 'package:super_fitness/config/validations/app_validations.dart';
 import 'package:super_fitness/core/utils/app_assets.dart';
 import 'package:super_fitness/core/utils/app_strings.dart';
+import 'package:super_fitness/core/utils/app_text_styles.dart';
 import 'package:super_fitness/core/widgets/app_scaffold.dart';
+import 'package:super_fitness/core/widgets/custom_app_bar.dart';
+import 'package:super_fitness/core/widgets/custom_glass_container.dart';
+import 'package:super_fitness/core/widgets/custom_text_field.dart';
 
 import '../view_model/register_view_model/register_cubit.dart';
 import '../view_model/register_view_model/register_event.dart';
+import '../widgets/social_login_buttons.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -20,6 +27,12 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> with UiEventHandler {
   late final StreamSubscription _uiEventSubscription;
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void initState() {
@@ -31,6 +44,10 @@ class _RegisterScreenState extends State<RegisterScreen> with UiEventHandler {
 
   @override
   void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
     _uiEventSubscription.cancel();
     super.dispose();
   }
@@ -39,12 +56,120 @@ class _RegisterScreenState extends State<RegisterScreen> with UiEventHandler {
   Widget build(BuildContext context) {
     return AppScaffold(
       backgroundImage: AppImages.authBackground,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.read<RegisterCubit>().doEvent(NextStepEvent());
-          },
-          child: Text(AppStrings.register.tr()),
+      appBar: CustomAppBar(onBackPressed: () => Navigator.pop(context)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 32.h),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    AppStrings.signupWelcome.tr(),
+                    style: AppTextStyles.white18400,
+                  ),
+                  SizedBox(height: 8.h),
+                  Text(
+                    AppStrings.signupCreateAccount.tr(),
+                    style: AppTextStyles.white20800,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.h),
+            CustomGlassContainer(
+              padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 24.h),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      AppStrings.register.tr(),
+                      style: AppTextStyles.white24700,
+                    ),
+                    SizedBox(height: 16.h),
+                    CustomTextField(
+                      controller: _firstNameController,
+                      hintText: AppStrings.firstName.tr(),
+                      prefixIconPath: AppIcons.person,
+                      validator: AppValidations.validateFirstName,
+                    ),
+                    SizedBox(height: 16.h),
+                    CustomTextField(
+                      controller: _lastNameController,
+                      hintText: AppStrings.lastName.tr(),
+                      prefixIconPath: AppIcons.person,
+                      validator: AppValidations.validateLastName,
+                    ),
+                    SizedBox(height: 16.h),
+                    CustomTextField(
+                      controller: _emailController,
+                      hintText: AppStrings.email.tr(),
+                      prefixIconPath: AppIcons.email,
+                      validator: AppValidations.validateEmail,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(height: 16.h),
+                    CustomTextField(
+                      controller: _passwordController,
+                      hintText: AppStrings.password.tr(),
+                      prefixIconPath: AppIcons.lock,
+                      obscureText: true,
+                      validator: AppValidations.validatePassword,
+                    ),
+                    SizedBox(height: 32.h),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          context.read<RegisterCubit>().doEvent(
+                            UpdateAccountInfoEvent(
+                              firstName: _firstNameController.text,
+                              lastName: _lastNameController.text,
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            ),
+                          );
+                          context.read<RegisterCubit>().doEvent(
+                            NextStepEvent(),
+                          );
+                        }
+                      },
+                      child: Text(
+                        AppStrings.register.tr(),
+                        style: AppTextStyles.white20500,
+                      ),
+                    ),
+                    SizedBox(height: 24.h),
+                    SocialLoginButtons(onAppleTap: () {}, onGoogleTap: () {}),
+                    SizedBox(height: 24.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          AppStrings.loginPrompt.tr(),
+                          style: AppTextStyles.white16500,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            AppStrings.login.tr(),
+                            style: AppTextStyles.primary16500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 24.h),
+          ],
         ),
       ),
     );
