020dcfe7f63c81eeaf78f0648a68867bc7e15056
Abdelmalek <m7244985@gmail.com>
2026-07-23 14:13:50 +0300

    feat: implement custom floating navigation bar with glassmorphism effect
    - Replaced standard Material NavigationBar with a custom floating design.
    - Added glassmorphism effect using BackdropFilter and blur (sigma 20).
    - Implemented custom _NavBarItem for enhanced UI control.
    - Updated AppTheme to support transparent navigation bar backgrounds.
    - Enabled extendBody in Scaffold for seamless background integration.

--- lib/features/main_layout/presentation/widgets/custom_svg_icon.dart
+++ lib/features/main_layout/presentation/widgets/custom_svg_icon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomSvgIcon extends StatelessWidget {
@@ -17,17 +16,14 @@ class CustomSvgIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final iconTheme = IconTheme.of(context);
-    final double effectiveSize = size ?? iconTheme.size ?? 40.0;
+    final double effectiveSize = size ?? iconTheme.size ?? 24.0;
     final Color effectiveColor = color ?? iconTheme.color ?? Colors.black;
 
-    return Padding(
-      padding: EdgeInsets.only(top: 12.h),
-      child: SvgPicture.asset(
-        iconPath,
-        width: effectiveSize,
-        height: effectiveSize,
-        colorFilter: ColorFilter.mode(effectiveColor, BlendMode.srcIn),
-      ),
+    return SvgPicture.asset(
+      iconPath,
+      width: effectiveSize,
+      height: effectiveSize,
+      colorFilter: ColorFilter.mode(effectiveColor, BlendMode.srcIn),
     );
   }
 }
